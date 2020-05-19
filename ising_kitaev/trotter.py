b1ca1692bc7d1b41d1d9795691574bac4be556ce
MatthieuDartiailh <marul@laposte.net>
2020-05-19 15:50:42 -0400

    trotter: fix implementation of the coupler interaction and add higher order trotter

--- ising_kitaev/trotter.py
+++ ising_kitaev/trotter.py
@@ -9,9 +9,9 @@ def pair_interaction(qc, q, i0, i1, coup):
     Since we normalize by J, the coupling is the trotter step.
 
     """
-    qc.cx(q[i0],q[i1])
-    qc.u1(coup,q[i1])
-    qc.cx(q[i0],q[i1])
+    qc.cx(q[i0], q[i1])
+    qc.u1(coup, q[i1])
+    qc.cx(q[i0], q[i1])
 
 
 def zeeman_term(qc, q, i, mag):
@@ -21,54 +21,127 @@ def zeeman_term(qc, q, i, mag):
     qc.rx(mag, q[i])
 
 
-def chain_hamiltonian(qc, q, coupling, zeeman, debug=False):
-    """Implement the chain Hamiltonian.
-
-    """
-    n = len(zeeman)
-    for j in range(0, n, 2):
-        pair_interaction(qc, q, j, j+1, coupling)
-    for j in range(1, n-1, 2):
-        pair_interaction(qc, q, j, j+1, coupling)
-        if(debug):
-            qc.barrier()
-    for j in range(n):
-        zeeman_term(qc, q, j, zeeman[j])
-    if(debug):
-        qc.barrier()
-
-
-def interaction_hamiltonian(qc, q, i0, i1, interaction):
+def coupler_hamiltonian(qc, q, i0, i1, interaction):
     """Implement the σ_z σ_z σ_x interaction with the coupler qubit.
 
     The coupler qubit is always the last coupling in the register.
 
     """
     coupler_index = len(q) - 1
-    qc.cx(q[i1],q[i0])
+    qc.cx(q[i1], q[i0])
     qc.h(q[coupler_index])
-    qc.cx(q[i0],q[coupler_index])
+    qc.cx(q[i0], q[coupler_index])
     qc.rz(-interaction, q[coupler_index])
-    qc.cx(q[i0],q[coupler_index])
+    qc.cx(q[i0], q[coupler_index])
     qc.h(q[coupler_index])
-    qc.cx(q[i1],q[i0])
+    qc.cx(q[i1], q[i0])
 
 
-def trotter_step(qc, q, coupling, zeeman, interaction):
+def interaction_hamiltonian(
+    qc, q, coupling, zeeman, coupler_interaction=None, debug=False
+):
+    """Implement the interaction term on the chain.
+
+    If a coupler_interaction is specified, the coupling between the two sites
+    closest to the middle of the chain is implemented through the coupler qubit
+    which by convention is always the last qubit of the register.
+
+    """
+    # NUmber of Ising sites in the chain
+    n = len(zeeman)
+    # Determine the index of the middle of the chain.
+    m = int(len(q) / 2 - 1)
+
+    # Build the list of function to call to add the interaction
+    to_call = [
+        (pair_interaction, (qc, q, j, j + 1, coupling))
+        if j != m
+        else (coupler_hamiltonian, (qc, q, j, j + 1, coupler_interaction))
+        for j in range(0, n - 1)
+    ]
+
+    for f, args in to_call[::2]:
+        f(*args)
+    if debug:
+        qc.barrier()
+    for f, args in to_call[1::2]:
+        f(*args)
+    if debug:
+        qc.barrier()
+
+
+def zeeman_hamiltonian(qc, q, zeeman, debug=False):
+    """Implement the Zeeman term on the chain.
+
+
+    """
+    n = len(zeeman)
+    for j in range(n):
+        zeeman_term(qc, q, j, zeeman[j])
+    if debug:
+        qc.barrier()
+
+
+def trotter_step_1(qc, q, coupling, zeeman, coupler, debug=False):
     """Add a trotter step to the circuit.
 
     """
-    chain_hamiltonian(qc, q, coupling, zeeman)
-    # Determine the index of the middle of the chain.
-    m = int(len(q)/2-1)
-    if (interaction!=0.0):
-        interaction_hamiltonian(qc, q, m, m+1,interaction)
+    zeeman_hamiltonian(qc, q, zeeman, debug)
+    interaction_hamiltonian(qc, q, coupling, zeeman, coupler, debug)
 
 
-def trotter(qc, q, zeeman, interaction, dt, nsteps, debug=False):
+def trotter_step_2(qc, q, coupling, zeeman, coupler, debug=False):
+    """Add a trotter step to the circuit.
+
+    """
+    zeeman_hamiltonian(qc, q, 0.5 * zeeman, debug)
+
+    interaction_hamiltonian(qc, q, coupling, zeeman, coupler, debug)
+
+    zeeman_hamiltonian(qc, q, 0.5 * zeeman, debug)
+
+
+def trotter_step_4(qc, q, coupling, zeeman, interaction, debug=False):
+    """Add a trotter step to the circuit.
+
+    """
+    s2 = 1 / (4 - 4 ** (1 / 3))
+    trotter_step_2(qc, q, s2 * coupling, s2 * zeeman, s2 * interaction, debug)
+    trotter_step_2(qc, q, s2 * coupling, s2 * zeeman, s2 * interaction, debug)
+    trotter_step_2(
+        qc,
+        q,
+        (1 - 4 * s2) * coupling,
+        (1 - 4 * s2) * zeeman,
+        (1 - 4 * s2) * interaction,
+        debug,
+    )
+    trotter_step_2(qc, q, s2 * coupling, s2 * zeeman, s2 * interaction, debug)
+    trotter_step_2(qc, q, s2 * coupling, s2 * zeeman, s2 * interaction, debug)
+
+
+def trotter_step_4_2(qc, q, coupling, zeeman, interaction, debug=False):
+    """Add a trotter step to the circuit.
+
+    Ths implementation can suffer from instabilities and is hence not used.
+
+    """
+    s2 = 1 / (2 - 2 ** (1 / 3))
+    trotter_step_2(qc, q, coupling, s2 * zeeman, interaction, debug)
+    trotter_step_2(qc, q, coupling, (1 - 2 * s2) * zeeman, interaction, debug)
+    trotter_step_2(qc, q, coupling, s2 * zeeman, interaction, debug)
+
+
+def trotter(qc, q, zeeman, interaction, dt, nsteps, order=1, debug=False):
     """Perform a Trotter evolution for a given number of timesteps.
 
     """
-    for i in range(nsteps):
-        trotter_step(qc, q, dt, zeeman*dt, interaction*dt)
+    if order == 1:
+        t_step = trotter_step_1
+    elif order == 2:
+        t_step = trotter_step_2
+    elif order == 4:
+        t_step = trotter_step_4
 
+    for i in range(nsteps):
+        t_step(qc, q, dt, zeeman * dt, interaction * dt, debug)
