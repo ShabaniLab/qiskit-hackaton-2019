32fb5e7eff2f09996c349cb8dea64bdab0359383
MatthieuDartiailh <marul@laposte.net>
2020-05-20 19:25:55 -0400

    scripts: update

--- scripts/adiabatic_passage.py
+++ scripts/adiabatic_passage.py
@@ -24,10 +24,10 @@ COUPLER_DURATION = 2
 
 SHOTS = 10000
 # 0.01, 0.02, 0.04, 0.05,
-TROTTER_STEPS = [0.05, 0.1, 0.2, 0.5]
+TROTTER_STEPS = [0.02, 0.05, 0.1, 0.2, 0.5]
 TROTTER_STEPS.reverse()
 
-RESULTS_PATH = "test_trotter.csv"
+RESULTS_PATH = "test.csv"
 
 USE_RESULTS = False
 
@@ -40,7 +40,7 @@ from qiskit.circuit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from ising_kitaev import (
     initialize_chain,
     initialize_coupler,
-    braid_chain,
+    move_chain,
     rotate_to_measurement_basis,
     add_measurement,
 )
@@ -48,7 +48,7 @@ import matplotlib.pyplot as plt
 from qiskit import Aer, execute
 
 
-def estimate_fidelity(timestep, use_logical_state, coupler, should_braid):
+def estimate_fidelity(timestep, coupler, strategy):
     """Estimate the fidelity of an operation.
 
     Parameters
@@ -56,44 +56,34 @@ def estimate_fidelity(timestep, use_logical_state, coupler, should_braid):
     timestep : float
         Timestep to use in the Trotter expansion.
 
-    use_logical_state : bool
-        Use logical state 0 as input otherwise use ↑↑↑
+    coupler : {"up", "down"}
+        Is the coupler up or down
 
-    coupler : bool
-        Is the coupler present or absent from the chain
-
-    should_braid : bool
-        When the coupler is present should we perform a braiding operation.
+    strategy : {"both", "single"}
+        Field update strategy.
 
     """
     initial_config = np.array(
         [H_FERRO] * FERRO_DOMAIN_SIZE + [H_PARA] * (NUMBER_OF_SITES - FERRO_DOMAIN_SIZE)
     )
-    qreg = QuantumRegister(NUMBER_OF_SITES + (1 if coupler else 0))
+    qreg = QuantumRegister(NUMBER_OF_SITES + 1)
     creg = ClassicalRegister(FERRO_DOMAIN_SIZE)
     qcirc = QuantumCircuit(qreg, creg)
-    initialize_chain(
-        qcirc, qreg, initial_config, "logical_zero" if use_logical_state else "up"
-    )
-    if coupler:
-        initialize_coupler(qcirc, qreg)
-    braid_chain(
+    initialize_chain(qcirc, qreg, initial_config, "up")
+    initialize_coupler(qcirc, qreg, coupler)
+    move_chain(
         qcirc,
         qreg,
-        np.pi,
         initial_config,
-        COUPLER_STRENGTH if coupler else None,
+        initial_config[::-1],
+        COUPLER_STRENGTH,
         GAP_FRACTION,
         MIN_INCREMENT,
         ZEEMAN_UPDATE_DELAY,
-        COUPLER_DURATION,
         int(round(ZEEMAN_UPDATE_DELAY / timestep)),
-        int(round(COUPLER_DURATION / timestep)),
-        method="both",
+        method=strategy,
     )
-    if use_logical_state:
-        rotate_to_measurement_basis(qcirc, qreg, list(range(FERRO_DOMAIN_SIZE)))
-    add_measurement(qcirc, qreg, creg, list(range(FERRO_DOMAIN_SIZE)))
+    add_measurement(qcirc, qreg, creg, [-2 - i for i in range(FERRO_DOMAIN_SIZE)])
 
     backend = Aer.get_backend("qasm_simulator")
     job = execute(
@@ -102,7 +92,7 @@ def estimate_fidelity(timestep, use_logical_state, coupler, should_braid):
 
     result = job.result()
     print(result.get_counts())
-    if should_braid:
+    if coupler == "-z":
         val = (result.get_counts().get("1" * FERRO_DOMAIN_SIZE, 0.0) / SHOTS) * 100
     else:
         val = (result.get_counts().get("0" * FERRO_DOMAIN_SIZE, 0.0) / SHOTS) * 100
@@ -111,27 +101,22 @@ def estimate_fidelity(timestep, use_logical_state, coupler, should_braid):
 
 if not os.path.isfile(RESULTS_PATH) or not USE_RESULTS:
 
-    results = np.zeros((5, len(TROTTER_STEPS)))
+    results = np.zeros((4, len(TROTTER_STEPS)))
     for i, step in enumerate(TROTTER_STEPS):
         print(f"Computation for Δt = {step}")
-        for j, (l, c, b) in enumerate(
-            zip(
-                (False, True, False, True, False),
-                (False, False, True, True, True),
-                (False, False, False, False, True),
-            )
+        for j, (coupler, strategy) in enumerate(
+            zip(("+z", "-z", "+z", "-z"), ("both", "both", "single", "single"))
         ):
-            print(f"Case: logical_state {l}, coupler {c}, braiding {b}")
-            results[j, i] = estimate_fidelity(step, l, c, b)
+            print(f"Computation for coupler: {coupler}, strategy: {strategy}")
+            results[j, i] = estimate_fidelity(step, coupler, strategy)
 
     df = pd.DataFrame(
         {
             "Steps": np.array(TROTTER_STEPS),
-            "No coupler - |↑↑↑>": results[0],
-            "No coupler - |0>": results[1],
-            # "Coupler - |↑↑↑>": results[2],
-            # "Coupler - |0>": results[3],
-            # "Braiding": results[4],
+            "Coupler: Up, Strategy: both": results[0],
+            "Coupler: Down, Strategy: both": results[1],
+            "Coupler: Up, Strategy: single": results[2],
+            "Coupler: Down, Strategy: single": results[3],
         }
     )
     if RESULTS_PATH:
